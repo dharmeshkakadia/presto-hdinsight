01fd7d9dc1d62757fd0fe79a3f6e4b96e2466bd9
Dharmesh Kakadia <dharmesh.kakadia@gmail.com>
2017-01-21 05:05:50 +0000

    add presto-cli

--- installpresto.sh
+++ installpresto.sh
@@ -2,6 +2,15 @@
 
 wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh && source /tmp/HDInsightUtilities-v01.sh && rm -f /tmp/HDInsightUtilities-v01.sh
 
+if [[ $shorthostname == workernode* || $shorthostname == wn* ]]; then
+  mkdir /var/lib/presto
+  chmod -R 777 /var/lib/presto/
+fi
+
+if [[ `hostname -f` == `get_primary_headnode` || `hostname -f` == `get_secondary_headnode` ]]; then
+  mkdir /var/lib/presto
+fi
+
 if [[ `hostname -f` == `get_primary_headnode` ]]; then
   which mvn &> /dev/null || sudo apt-get -y -qq install maven
   mkdir /var/lib/presto
@@ -18,8 +27,21 @@ if [[ `hostname -f` == `get_primary_headnode` ]]; then
   slider create presto1 --template appConfig-default.json --resources resources-default.json
 fi
 
-if [[ $shorthostname == workernode* || $shorthostname == wn* ]]; then
-  mkdir /var/lib/presto
-  chmod -R 777 /var/lib/presto/
+if [[ `hostname -f` == `get_primary_headnode` || `hostname -f` == `get_secondary_headnode` ]]; then
+  cd /var/lib/presto
+  wget https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/0.163/presto-cli-0.163-executable.jar -O presto-cli
+  chmod +x presto-cli
+
+  until slider registry  --name presto1 --getexp presto ; do
+    echo "waiting for presto to start.."
+    sleep 10
+  done
+
+  cat > /usr/local/bin/presto <<EOF
+#!/bin/bash
+/var/lib/presto/presto-cli --server $(slider registry  --name presto1 --getexp presto |  grep value | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*:[0-9]*") --catalog hive "\$@"
+EOF
+  
+  chmod +x /usr/local/bin/presto
 fi
 
