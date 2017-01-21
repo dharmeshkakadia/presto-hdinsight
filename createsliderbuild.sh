21a4067c148d49056b797bcb799a2fe2a729d011
Dharmesh Kakadia <dharmesh.kakadia@gmail.com>
2017-01-21 01:38:34 +0000

    WASB patching

--- createsliderbuild.sh
+++ createsliderbuild.sh
@@ -1,20 +1,24 @@
 mkdir build
 cd build
 
-git clone https://github.com/prestodb/presto-yarn
-#update the presto.version
-mvn clean package -f presto-yarn/pom.xml
+wget https://github.com/prestodb/presto-yarn/archive/master.tar.gz -O presto-yarn.tar.gz
+tar xzf presto-yarn.tar.gz
+mvn -Dpresto.version=0.163 clean package -f presto-yarn-master/pom.xml
+unzip presto-yarn-master/presto-yarn-package/target/presto-yarn-package-*.zip -d presto-yarn-package
+tar xzf presto-yarn-package/package/files/presto-server-*.tar.gz
 
-unzip presto-yarn/presto-yarn-package/target/presto-yarn-package-1.4-SNAPSHOT-0.163.zip -d presto-yarn-package
-tar xzf presto-yarn-package/package/files/presto-server-0.163.tar.gz
-rm presto-server-0.163/plugin/hive-hadoop2/hadoop-apache2-0.10.jar
+wget https://github.com/dharmeshkakadia/presto-hadoop-apache2/archive/master.tar.gz -O presto-hadoop-apache2.tar.gz
+mvn clean package -f presto-hadoop-apache2-master/pom.xml
+rm presto-server-*/plugin/hive-hadoop2/hadoop-apache2-*.jar
+cp presto-hadoop-apache2-master/target/hadoop-apache2-0.11-SNAPSHOT.jar presto-server-*/plugin/hive-hadoop2/
 
-git clone https://github.com/prestodb/presto-hadoop-apache2
-#Apply WASB patch
-mvn clean package -f presto-hadoop-apache2/pom.xml
-cp presto-hadoop-apache2/target/hadoop-apache2-0.11-SNAPSHOT.jar presto-server-0.163/plugin/hive-hadoop2/
+cp /usr/hdp/current/hadoop-client/hadoop-azure-*.jar  presto-server-*/plugin/hive-hadoop2/
+cp /usr/hdp/current/hadoop-yarn-client/lib/jetty-util-*.hwx.jar  presto-server-*/plugin/hive-hadoop2/
+cp /usr/hdp/current/hadoop-yarn-client/lib/azure-storage-*.jar presto-server-*/plugin/hive-hadoop2/
 
-tar cvf presto-server-0.163.tar.gz presto-server-0.163/
-cp presto-server-0.163.tar.gz presto-yarn-package/package/files/
+tar czvf presto-server-0.163.tar.gz presto-server-*/
+rm presto-yarn-package/package/files/presto-server*.tar.gz
+cp presto-server-*.tar.gz presto-yarn-package/package/files/
 cd presto-yarn-package
-zip -r ../../presto-yarn-package-1.4-SNAPSHOT-0.163.zip .
+zip -r ../presto-yarn-package.zip .
+cd ..
