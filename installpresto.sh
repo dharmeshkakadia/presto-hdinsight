1d127789b55d82106cd270f52f5e45391f3687d4
Dharmesh Kakadia <dharmesh.kakadia@gmail.com>
2017-01-21 03:01:39 +0000

    config generation

--- installpresto.sh
+++ installpresto.sh
@@ -11,6 +11,8 @@ if [[ `hostname -f` == `get_primary_headnode` ]]; then
   cd presto-hdinsight-master
   ./createsliderbuild.sh
   slider package --install --name presto1 --package build/presto-yarn-package.zip
+  ./createconfigs.sh
+  slider create presto1 --template appConfig-default.json --resources resources-default.json
 fi
 
 if [[ $shorthostname == workernode* || $shorthostname == wn* ]]; then
