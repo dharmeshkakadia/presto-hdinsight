d7ec633f7afea1ae66fe9ff8cb99f2380e4f5710
Dharmesh Kakadia <dharmeshkakadia@users.noreply.github.com>
2017-01-22 14:20:52 -0800

    simplify

--- installpresto.sh
+++ installpresto.sh
@@ -2,18 +2,11 @@
 
 wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh && source /tmp/HDInsightUtilities-v01.sh && rm -f /tmp/HDInsightUtilities-v01.sh
 
-if [[ $shorthostname == workernode* || $shorthostname == wn* ]]; then
-  mkdir /var/lib/presto
-  chmod -R 777 /var/lib/presto/
-fi
-
-if [[ `hostname -f` == `get_primary_headnode` || `hostname -f` == `get_secondary_headnode` ]]; then
-  mkdir /var/lib/presto
-fi
+mkdir /var/lib/presto
+chmod -R 777 /var/lib/presto/
 
 if [[ `hostname -f` == `get_primary_headnode` ]]; then
   which mvn &> /dev/null || sudo apt-get -y -qq install maven
-  mkdir /var/lib/presto
   cd /var/lib/presto
   wget https://github.com/dharmeshkakadia/presto-hdinsight/archive/master.tar.gz -O presto-hdinsight.tar.gz
   tar xzf presto-hdinsight.tar.gz
