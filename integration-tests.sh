a5b551cc4f14839e636634f9a36578c2c53f7e76
Dharmesh Kakadia <dharmeshkakadia@users.noreply.github.com>
2017-04-07 10:59:31 -0700

    format

--- integration-tests.sh
+++ integration-tests.sh
@@ -2,21 +2,21 @@
 set -eux
 
 hivetest(){
-	result=$(tempfile)
-	presto --schema default --execute "select count(*) from hivesampletable" > $result
+  result=$(tempfile)
+  presto --schema default --execute "select count(*) from hivesampletable" > $result
 
-	# There are quotes in the result "59793"
-	if [[ $(cat $result) == "\"59793\"" ]] ; then
-	  echo "Hive connector test passed"
-	else
-	  echo "Hive connector test failed: expected \"59793\" , but got $(cat $result)"
-	  exit 1
-	fi
+  # There are quotes in the result "59793"
+  if [[ $(cat $result) == "\"59793\"" ]] ; then
+    echo "Hive connector test passed"
+  else
+    echo "Hive connector test failed: expected \"59793\" , but got $(cat $result)"
+    exit 1
+  fi
 }
 
 until [[ $(presto --schema default --execute 'select * from system.runtime.nodes') ]]; do
-	echo "waiting for presto workers to join.."
-	sleep 5
+  echo "waiting for presto workers to join.."
+  sleep 5
 done
 
 hivetest
