98fa4daa0bf8fc34ccc74f47341646c1940820df
Dharmesh Kakadia <dharmeshkakadia@users.noreply.github.com>
2017-04-06 18:26:01 -0700

    typo

--- integration-tests.sh
+++ integration-tests.sh
@@ -8,7 +8,7 @@ presto --schema default --execute "select count(*) from hivesampletable"
 if [ $(cat $result) == "\"59793\"" ]; then
   echo "Hive connector test passed"
 else
-  echo "Hive connector test failed " expected \"59793\" , but got $(cat $result)"
+  echo "Hive connector test failed: expected \"59793\" , but got $(cat $result)"
   exit 1
 fi  
   
